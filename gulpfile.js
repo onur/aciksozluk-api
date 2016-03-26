5bb443bff331433d66c75eb6453e99ba48f548a9
Onur Aslan <onuraslan@gmail.com>
2016-03-26 11:29:29 +0200

    Remove array-bracket-spacing
    array-bracket-spacing is set to never

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,16 @@ var eslint = require('gulp-eslint');
 var mocha = require('gulp-mocha');
 
 gulp.task('lint', function() {
-    return gulp.src([ '**/*.js', '!node_modules/**', '!coverage/**' ])
+    return gulp.src(['**/*.js', '!node_modules/**', '!coverage/**'])
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failOnError());
 });
 
 gulp.task('test', function() {
-    return gulp.src([ 'test/**/*.js' ], { read: false })
+    return gulp.src(['test/**/*.js'], { read: false })
         .pipe(mocha({ reporter: 'spec' }))
         .pipe(eslint.failOnError());
 });
 
-gulp.task('default', [ 'test', 'lint' ]);
+gulp.task('default', ['test', 'lint']);
